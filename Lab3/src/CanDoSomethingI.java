93fc0017af33827c9d6a054fa8bd1290e9015dd6
Lestsy <96525264+Lestsy@users.noreply.github.com>
2022-03-15 15:38:33 +0300

    Delete Lab3 directory

--- Lab3/src/CanDoSomethingI.java
+++
@@ -1,6 +0,0 @@
-public interface CanDoSomethingI {
-    public void doSomething(Backpack backpack);
-    public void doAction(ActionType... actionType);
-    public void doAction(String message, ActionType... actionType);
-    public void doAction(String message);
-}
